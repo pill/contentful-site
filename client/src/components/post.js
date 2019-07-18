ddbe42c5eab5fcd90fcab9ff00435aeff9c545fa
Phil <phil.avery@gmail.com>
2019-07-18 08:25:26 -0400

    added post call

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -2,6 +2,19 @@ import React, {useEffect, useState}  from "react"
 import client from "../api"
 
 export default function Post(props) {
+
+  const [entry, setEntry] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await client.getEntry(props.id)
+      console.log('res', res)
+      setEntry(res)
+    }
+    fetchData()
+  }, [])
+
+
   return (
     <div>
       <h1>Post</h1>
