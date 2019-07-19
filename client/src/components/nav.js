11e326bb136ad77c0109cf371220f06849d740e7
Phil <phil.avery@gmail.com>
2019-07-19 08:22:18 -0400

    add nav

---
+++ client/src/components/nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default () => {
+  return (
+    <div>
+      <a href="/blog">blog</a>
+      &nbsp;
+      <a href="/work">work</a>
+    </div>
+  )
+}
